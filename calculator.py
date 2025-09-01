0c5de92f16a76b6bbfb8230f860511c9b0aeb64b
ritik-dev2000 <ritik@sav.com>
2025-09-02 01:21:54 +0530

    addition

--- calculator.py
+++ calculator.py
@@ -2,9 +2,6 @@
 class Calculator:
     def add(self, a, b):
         return a + b
-    def subtract(self, a, b):
-        return a - b
 calc  = Calculator()
 a,b =int(input("First")),int(input("Second"))
 print("Sum:",calc.add(a,b))
-print("Difference:",calc.subtract(a,b))

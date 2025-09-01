57d5f9d7f9b8b77436a2e7733bc870b7166c965e
ritik-dev2000 <ritik@sav.com>
2025-09-02 01:56:30 +0530

    multiplication

--- calculator.py
+++ calculator.py
@@ -2,6 +2,13 @@
 class Calculator:
     def add(self, a, b):
         return a + b
+<<<<<<< Updated upstream
+=======
+    def subtract(self, a, b):
+        return a - b
+    def multiply(self, a, b):
+        return a * b    
+>>>>>>> Stashed changes
 calc  = Calculator()
 a,b =int(input("First")),int(input("Second"))
 print("Sum:",calc.add(a,b))
